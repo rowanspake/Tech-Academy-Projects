e8971fa07ebedc2edb7e57efb278072e843f1b13
rowanspake <109189967+rowanspake@users.noreply.github.com>
2023-05-31 00:20:27 -0700

    Moved Files

--- showcase-projects/concert-tracker-webapp/ConcertTracker/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConcerttrackerConfig(AppConfig):
-    name = 'ConcertTracker'
--- showcase-projects/theatre-website/TheatreCMS3/Scripts/Site.js
+++
@@ -1,8 +0,0 @@
-﻿// This is for the SignIn.cshtml view to count the number of Developers on the page.
-
-document.addEventListener("DOMContentLoaded", function () {
-    var numPersonsElement = document.getElementById("NumPersons");
-    var personsList = document.getElementById("PersonList");
-    var numPersons = personsList.getElementsByTagName("p").length;
-    numPersonsElement.textContent = numPersons;
-});
